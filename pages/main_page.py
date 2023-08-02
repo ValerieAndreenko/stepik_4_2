0f4b56ae428abb001652c46562fa51e3e74e1de0
AstridCrown <astridcrown@mail.ru>
2023-08-02 16:06:52 +0300

    Exception cauted

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        # login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # login_link.click()
     def go_to_basket(self):
         basket_in = self.browser.find_element(By.CLASS_NAME, "btn.btn-default")
         basket_in.click()
